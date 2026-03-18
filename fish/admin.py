316349830f5795f4eac25405eac1dc19adb9fc67
mdkaif2k3 <mohamkaif2003@gmail.com>
2026-03-18 11:17:21 +0530

    Delete fish directory

--- fish/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserLogin, FishSpecies, FoodType, Recommendation
-
-# Register your models here.
-admin.site.register(UserLogin)
-admin.site.register(FishSpecies)
-admin.site.register(FoodType)
-admin.site.register(Recommendation)
--- fish/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FishConfig(AppConfig):
-    name = 'fish'
